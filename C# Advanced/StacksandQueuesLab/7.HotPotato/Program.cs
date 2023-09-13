edf42d9d5ab7e0c19a43717c395392b4fc6dc8aa
paktozi <130216112+paktozi@users.noreply.github.com>
2023-09-13 19:07:05 +0300

    Add files via upload

---
+++ C# Advanced/StacksandQueuesLab/7.HotPotato/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _7.HotPotato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>(Console.ReadLine().Split());
+            int toss = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            while (queue.Count != 1)
+            {
+                count++;
+                string kid = queue.Dequeue();
+                if (count != toss)
+                {
+                    queue.Enqueue(kid);
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {kid}");
+                    count = 0;
+                }
+            }
+            Console.WriteLine($"Last is {queue.Peek()}");
+        }
+    }
+}
---
+++ C# Advanced/StacksandQueuesLab/8.TrafficJam/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _8.TrafficJam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfCars = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+            int count = 0;
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command != "green")
+                {
+                    cars.Enqueue(command);
+                }
+                else 
+                {
+                    for (int i = 0; i < numOfCars&&cars.Count>0; i++)
+                    {
+                        Console.WriteLine($"{cars.Peek()} passed!");
+                        cars.Dequeue();
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
