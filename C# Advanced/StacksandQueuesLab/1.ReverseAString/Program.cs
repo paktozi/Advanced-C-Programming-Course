6e872443fd9ea57591b13e312f84adc9f0fbfa04
paktozi <130216112+paktozi@users.noreply.github.com>
2023-09-13 19:24:03 +0300

    Add files via upload

---
+++ C# Advanced/StacksandQueuesLab/1.ReverseAString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _1.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Stack<char> stack = new Stack<char>(word);
+       
+            foreach (var c in stack)
+            {
+                Console.Write(c);
+            }
+        }
+    }
+}
