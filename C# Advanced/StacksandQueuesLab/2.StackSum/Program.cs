074769f8e58cd6f184e8a53d441cf05fd86f49f3
paktozi <130216112+paktozi@users.noreply.github.com>
2023-09-18 18:15:54 +0300

    Add files via upload

---
+++ C# Advanced/StacksandQueuesLab/2.StackSum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _2.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            string input;
+
+            while ((input = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] command = input.Split();
+
+                if (command.Contains("add"))
+                {
+                    stack.Push(int.Parse(command[1]));
+                    stack.Push(int.Parse(command[2]));
+                }
+                else if (command.Contains("remove"))
+                {
+                    int n = int.Parse(command[1]);
+
+                    if (stack.Count-n >=0)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+            }
+            if(stack.Count > 0)
+            {
+                Console.WriteLine($"Sum: {stack.Sum()}");
+            }    
+        }
+    }
+}
