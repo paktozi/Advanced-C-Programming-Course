650cdf634287d588a85d8e74ec0773bf2d9504dd
paktozi <130216112+paktozi@users.noreply.github.com>
2023-10-01 00:14:23 +0300

    Add files via upload

---
+++ C# Advanced/FunctionalProgrammingExercise/01.ActionPrint/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+
+Action<string[]> print = (string[] x) =>Console.WriteLine(string.Join(Environment.NewLine,x));   
+string[] name = Console.ReadLine()
+    .Split();
+print (name);
---
+++ C# Advanced/FunctionalProgrammingExercise/02.KnightsofHonor/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+Action<string[]> print = x =>
+{
+    foreach (var item in x)
+    {
+        Console.WriteLine($"Sir {item}");
+    }
+};
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+print(names);
