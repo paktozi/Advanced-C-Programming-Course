e72752e468dca8e3123a9f90986d74f15e7c8664
paktozi <130216112+paktozi@users.noreply.github.com>
2023-10-28 23:46:14 +0300

    Add files via upload

---
+++ C#OOP/Exercise-Inheritance/Animals/Person/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+       
+        int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set 
+            { 
+                if(value >0)
+                {
+                   age = value;
+                }
+            }
+        }
+
+        public override string ToString() => $"Name: {this.Name}, Age: {this.Age}";
+    }
+}
