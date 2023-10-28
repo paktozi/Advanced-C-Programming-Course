c8fe3b99fef39675d6547da2e581e499b9153766
paktozi <130216112+paktozi@users.noreply.github.com>
2023-10-28 23:45:06 +0300

    Add files via upload

---
+++ C#OOP/Exercise-Inheritance/Animals/NeedForSpeed/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            StoreVehicle<object> vehicle = new();
+           
+            Car car = new(10, 20);
+            Motorcycle motorcycle = new(8, 4);
+
+            vehicle.AddToList(car);
+           vehicle.AddToList(motorcycle);
+
+            vehicle.PrintVehicle();
+        }
+    }
+}
---
+++ C#OOP/Exercise-Inheritance/Animals/NeedForSpeed/StoreVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    internal class StoreVehicle<T>
+    {
+        public List<T> VehicleList = new();
+
+
+        public void AddToList(T item)
+        {
+            VehicleList.Add(item);
+        }
+        public void PrintVehicle()
+        {
+            foreach (var item in VehicleList)
+            {
+               Console.WriteLine(item);
+            }
+        }
+
+    }
+}
