05de449e260738c61c010ca1fb77fb80c9eeba72
paktozi <130216112+paktozi@users.noreply.github.com>
2023-10-28 23:42:21 +0300

    Add files via upload

---
+++ C#OOP/Exercise-Inheritance/Animals/Animals/Tomcat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        private const string Gender = "Male";
+        
+        public Tomcat(string name, int age):base(name,age,Gender)
+        {
+        }
+        public override string ProduceSound() => "MEOW";       
+    }
+}
