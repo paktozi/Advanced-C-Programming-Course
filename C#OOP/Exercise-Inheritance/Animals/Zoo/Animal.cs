b5467bbc06c9ea325d8335892fa6cc163fa7a54d
paktozi <130216112+paktozi@users.noreply.github.com>
2023-10-28 23:48:31 +0300

    Add files via upload

---
+++ C#OOP/Exercise-Inheritance/Animals/Zoo/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    internal class Animal
+    {
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+
+    }
+}
