fc5b8f63c56234e6e0336bc21b9d6d0861596f19
paktozi <130216112+paktozi@users.noreply.github.com>
2023-10-28 23:47:49 +0300

    Add files via upload

---
+++ C#OOP/Exercise-Inheritance/Animals/Restaurant/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("OK");
+
+            Cake cake = new("cake");
+
+            Console.WriteLine($"{cake.Name} {cake.Price} {cake.Grams} {cake.Calories}");
+        }
+    }
+}
