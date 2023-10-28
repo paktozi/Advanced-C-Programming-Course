992d1ae03f7a6cc94a52efc38a19415a997ab895
paktozi <130216112+paktozi@users.noreply.github.com>
2023-10-28 23:46:59 +0300

    Add files via upload

---
+++ C#OOP/Exercise-Inheritance/Animals/PlayersAndMonsters/SoulMaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayersAndMonsters
+{
+    internal class SoulMaster:DarkWizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        {
+
+        }
+    }
+}
